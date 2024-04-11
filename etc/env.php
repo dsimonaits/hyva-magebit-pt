eedeac91281b3e01acf1913732f9e6dac91800b1
Deniss Simonaits <deniss.simonaits@magebit.com>
2024-04-11 15:49:11 +0300

    Starting Practical task

---
+++ etc/env.php
@@ -0,0 +1,89 @@
+<?php
+return [
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'backend' => [
+        'frontName' => 'admin_6735t31'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'USuzeHSLZcNQLb89ZrKPFXUmrLo7k3vy'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '066_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '066_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'config' => [
+        'async' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'base64X2OhhW92QmyUVNNFyW7A0s+qcKtf9cmv2Z1xHdKmAxQ='
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'hyva_magebit',
+                'username' => 'root',
+                'password' => 'option123',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'graphql_query_resolver_result' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'hyva-magebit.local'
+    ],
+    'install' => [
+        'date' => 'Thu, 11 Apr 2024 12:22:00 +0000'
+    ]
+];
